69d53bf5dc22a7759d5ac51e6ac151b8339073c2
Eason Chiang <ishiu.chiang@gmail.com>
2019-08-03 00:25:27 +0800

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `music-player`,
+        short_name: `mp3-player`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFF498`,
+        theme_color: `#F5CD40`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mp3-player-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
